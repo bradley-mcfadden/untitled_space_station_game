0498d2feb59f5f0cad00893d6371563760945d2f
Bradley McFadden <bmcfadden00@gmail.com>
2019-05-11 18:53:39 -0400

    Added UnsortedSet class

--- Gun Testing/Scripts/UnsortedSet.gd
+++ Gun Testing/Scripts/UnsortedSet.gd
@@ -1,2 +1,22 @@
 extends Node
 
+var data = []
+
+func contains(t) -> bool:
+	for i in range(data.size()):
+		if data[i].equals(t):
+			return true
+	return false
+
+func add(t):
+	if contains(t):
+		return
+	data.append(t)
+	
+func to_string() -> String:
+	var s = "["
+	for i in range(data.size()):
+		s += data[i]
+	s += "]"
+	return s
+
