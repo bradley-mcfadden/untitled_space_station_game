3addfe6b4cdacb05563ce2037b46fde558f87705
Bradley McFadden <bmcfadden00@gmail.com>
2019-05-16 22:03:32 -0400

    Event handlers for damage taken, swapping weapons now works

--- Gun Testing/Scripts/HUD.gd
+++ Gun Testing/Scripts/HUD.gd
@@ -1,26 +1,22 @@
 extends CanvasLayer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	$HealthBar.value = get_parent().MAX_HEALTH
+	$CoinCountLabel.text = str(get_parent().coins)
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func healthUpdate(health:int):
+	$HealthBar.value = health
 
 func _on_weaponSwap(weapon:Gun):
 	if weapon is AnimatedSprite:
-		print("yes")
+		#print("yes")
 		var frames = weapon.get_sprite_frames()
 		var def = frames.get_frame("default",0)
 		$WeaponRect.texture = def
 	$AmmoLabel.text = str(weapon.actualBullets) +"\n"+str(weapon.clipSize)
 	$WeaponLabel.text = weapon.gunName
-func _on_updateUI(weapon:Gun):
+	
+func _on_updateGun(weapon:Gun):
 	$AmmoLabel.text = str(weapon.actualBullets) +"\n"+str(weapon.clipSize)
 	
