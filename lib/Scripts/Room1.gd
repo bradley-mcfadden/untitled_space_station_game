c8eaf5d5b14b500a81b7e89ae648d63cbb63139a
Bradley McFadden <bmcfadden00@gmail.com>
2019-06-23 19:40:58 -0400

    Gave a cleared property.

--- lib/Scripts/Room1.gd
+++ lib/Scripts/Room1.gd
@@ -1,6 +1,6 @@
 extends TileMap
 export var enemies = []
-
+var cleared = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
